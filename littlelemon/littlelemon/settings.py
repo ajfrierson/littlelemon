1d7527b1b1163f897ea22abb1bbc3e5d0445b93e
Alvin Frierson <afrierson@entertainmentcommunity.org>
2025-04-16 13:20:14 -0700

    changed the name of the app to restaurant

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation',  # Custom app for reservations
+    'restaurant',  # Custom app for reservations
 ]
 
 MIDDLEWARE = [
